c1b4b58cc4dfc70be7c5e8d50cd3ec2626ea5155
Rana Singh <90957952+ranasingh1@users.noreply.github.com>
2024-04-14 18:01:01 +0530

    Time: 11 ms (7.84%) | Memory: 44.6 MB (96.07%) - LeetSync

---
+++ 455-assign-cookies/assign-cookies.java
@@ -0,0 +1,68 @@
+class Solution {
+    public int findContentChildren(int[] g, int[] s) {
+        int i = 0, j = 0, answer = 0;
+      
+        sort(g, 0, g.length-1);
+        sort(s, 0, s.length-1);
+     
+        while (i < g.length && j < s.length) {
+            if (g[i] <= s[j]) {
+                answer++;
+                i++;
+                j++;
+            } else {
+                i++;
+            }
+        }
+
+        return answer;
+    }
+
+    void sort(int arr[], int l, int r) {
+        if (l < r) {
+            int m = (l + r) / 2;
+            sort(arr, l, m);
+            sort(arr, m + 1, r);
+            merge(arr, l, m, r);
+        }
+    }
+
+
+    void merge(int arr[], int l, int m, int r) {
+        int n1 = m - l + 1;
+        int n2 = r - m;
+
+        int L[] = new int[n1];
+        int R[] = new int[n2];
+
+        for (int i = 0; i < n1; ++i)
+            L[i] = arr[l + i];
+        for (int j = 0; j < n2; ++j)
+            R[j] = arr[m + 1 + j];
+
+        int i = 0, j = 0;
+
+        int k = l;
+        while (i < n1 && j < n2) {
+            if (L[i] >= R[j]) {
+                arr[k] = L[i];
+                i++;
+            } else {
+                arr[k] = R[j];
+                j++;
+            }
+            k++;
+        }
+        while (i < n1) {
+            arr[k] = L[i];
+            i++;
+            k++;
+        }
+
+        while (j < n2) {
+            arr[k] = R[j];
+            j++;
+            k++;
+        }
+    }
+}
