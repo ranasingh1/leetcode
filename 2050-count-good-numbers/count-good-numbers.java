186636002f67792041019835ad9e3d9ff27850d4
Rana Singh <90957952+ranasingh1@users.noreply.github.com>
2024-06-20 08:20:41 +0530

    Time: 0 ms (100.00%) | Memory: 39.9 MB (96.06%) - LeetSync

---
+++ 2050-count-good-numbers/count-good-numbers.java
@@ -0,0 +1,23 @@
+class Solution {
+    
+     public long MOD = 1_000_000_007;
+
+    public int countGoodNumbers(long n) {
+        long odd = n/2;
+        long even = (n+1)/2;
+        return (int)(pow(5,even) * pow(4,odd) % MOD);
+    }
+
+     public long pow(long x, long n){
+         if(n==0){
+             return 1;
+         }
+
+        long temp = pow(x, n/2);
+        if(n%2==1){
+            return (x*temp*temp)%MOD;
+        }else{
+               return (temp*temp)%MOD;
+        }
+     }
+}
